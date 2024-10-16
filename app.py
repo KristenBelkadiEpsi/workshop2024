b025d35f658ef69277edc43a0df5416edc625995
Aubin KAMANDA <kamandaaubin36@gmail.com>
2024-10-16 11:16:13 +0200

    ajout date

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from detoxify import Detoxify
 import mariadb.connections
 import pandas as pd
-from flask import Flask, request, make_response
-from flask_cors import CORS
+from flask import Flask, request, make_response, g
 import mariadb
 import os
 import dotenv
 from models.post import Post
 import json
-import datetime
+
 dotenv.load_dotenv()
 model = Detoxify("multilingual")
 toxicity_limit = 0.6
 reset_mode = bool(os.environ.get("RESET_MODE"))
 app = Flask(__name__)
-CORS(app)
-
 config = {
     "host": os.environ.get("DB_HOST"),
     "port": int(os.environ.get("DB_PORT")),
@@ -36,7 +33,8 @@ if reset_mode:
 conn.cursor().execute(
     """CREATE TABLE IF NOT EXISTS Post (
                             id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                            text TEXT NOT NULL
+                            text TEXT NOT NULL,
+                            datecreated DATETIME DEFAULT CURRENT_TIMESTAMP
 )"""
 )
 
@@ -44,11 +42,13 @@ conn.cursor().execute(
 @app.route("/api/post", methods=["GET"])
 def get_post():
     cursor = conn.cursor()
-    cursor.execute("SELECT id, text FROM Post")
+    cursor.execute("SELECT id, text, datecreated FROM Post")
     rows = cursor.fetchall()
+    print("-" * 1000)
     posts = []
     for row in rows:
-        posts.append(Post(row[0], row[1]))
+        print(row)
+        posts.append(Post(row[0], row[1], row[2]))
     return (json.dumps(posts, default=lambda p: p.__dict__), 200)
 
 
@@ -56,11 +56,11 @@ def get_post():
 def create_post():
     request_json = request.json
     if request_json is None:
-        return make_response("", 400)
+        return make_response("", 404)
     else:
         text = request_json["text"]
         if text is None:
-            return make_response("", 400)
+            return make_response("", 404)
         else:
             predict = model.predict([text])
             data_frame = pd.DataFrame(predict).round(5)
@@ -70,11 +70,13 @@ def create_post():
                 cursor.execute("INSERT INTO Post (text) VALUES (?)", (text,))
                 cursor.execute("SELECT LAST_INSERT_ID()")
                 id = cursor.fetchone()[0]
+                cursor.execute("SELECT datecreated FROM Post WHERE id = ?", (id,))
+                datecreated = cursor.fetchone()[0]
                 cursor.close()
-                post = Post(id, text)
+                post = Post(id, text, datecreated)
                 return make_response(json.dumps(post.__dict__), 200)
             else:
-                return make_response("", 403)
+                return make_response("", 400)
 
 
 if __name__ == "__main__":
